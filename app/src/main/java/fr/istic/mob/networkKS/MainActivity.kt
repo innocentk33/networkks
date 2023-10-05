d7c5617df0df1e18e39ad4ba9a3a44e3af769a0d
Wilfried Amos SEH <amos.seh@mtit-us.com>
2023-10-05 14:51:38 +0000

    canvas

--- app/src/main/java/fr/istic/mob/networkKS/MainActivity.kt
+++ app/src/main/java/fr/istic/mob/networkKS/MainActivity.kt
@@ -1,20 +1,28 @@
 package fr.istic.mob.networkKS
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ondraw
+
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+        setContentView(myview(this));
+
     }
 
    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -23,6 +31,20 @@ class MainActivity : AppCompatActivity() {
           inflater.inflate(R.menu.nav_bar, menu)
           return true
       }
+
+
+    internal class myview(context: Context?) : View(context) {
+        override fun onDraw(canvas: Canvas) {
+            super.onDraw(canvas)
+            val x = 800
+            val y = 80
+            val radius = 40
+            val paint = Paint()
+            // Use Color.parseColor to define HTML colors
+            paint.color = Color.parseColor("#CD5C5C")
+            canvas.drawCircle(x.toFloat(), y.toFloat(), radius.toFloat(), paint)
+        }
+    }
 
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
