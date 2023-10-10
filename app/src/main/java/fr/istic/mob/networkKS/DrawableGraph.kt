113c87bebff1fa6fadc7987272eb5f344ad5a365
Innocent Kacou <innocentk33@gmail.com>
2023-10-10 16:35:25 +0200

    somme change

--- app/src/main/java/fr/istic/mob/networkKS/DrawableGraph.kt
+++ app/src/main/java/fr/istic/mob/networkKS/DrawableGraph.kt
@@ -2,14 +2,15 @@ package fr.istic.mob.networkKS
 
 import android.graphics.Canvas
 import android.graphics.ColorFilter
+import android.graphics.Paint
 import android.graphics.drawable.Drawable
 import android.view.View
 
 class DrawableGraph :Drawable() {
     override fun draw(canvas: Canvas) {
-        TODO("Not yet implemented")
+       // TODO("Not yet implemented")
+        canvas.drawCircle(100f,100f,40f, Paint())
     }
-
     override fun setAlpha(alpha: Int) {
         TODO("Not yet implemented")
     }
